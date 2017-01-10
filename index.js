6b7abf3e3a262d0412d79176e17828da381a78fa
jconsigliere <jconsigliere@gmx.com>
2017-01-10 11:10:13 +0000

    Feat: integrate cloudinary service

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
 const models = require('./lib/models');
+const CloudinaryService = require('./lib/services/cloudinary_service.js');
 
-module.exports = { models };
+module.exports = { models, CloudinaryService };
